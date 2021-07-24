12af6d1aaf6da37c1369e2e38845b115ad566296
codycc <linkmecody@gmail.com>
2021-07-24 14:48:12 -0400

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Cody Condon on 2021-07-24.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
